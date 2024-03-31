e79895c96ae032350bf9718c5642a2523388a71d
AdityaRam24 <89917849+AdityaRam24@users.noreply.github.com>
2024-03-31 17:23:41 +0530

    Add files via upload

---
+++ mytt database.sql
@@ -0,0 +1,438 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 31, 2024 at 11:58 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `mytt`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `A_name` varchar(20) NOT NULL,
+  `A_password` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`A_name`, `A_password`) VALUES
+('Rohit Barve', 'Rohit@123');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `coordinator`
+--
+
+CREATE TABLE `coordinator` (
+  `C_name` varchar(20) NOT NULL,
+  `C_password` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `department`
+--
+
+CREATE TABLE `department` (
+  `dept_name` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `department`
+--
+
+INSERT INTO `department` (`dept_name`) VALUES
+('INFT');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `dept_contains`
+--
+
+CREATE TABLE `dept_contains` (
+  `D_name` varchar(10) NOT NULL,
+  `Subject_name` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `is_alloted_to`
+--
+
+CREATE TABLE `is_alloted_to` (
+  `t_name` varchar(20) DEFAULT NULL,
+  `S_id` varchar(5) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `is_alloted_to`
+--
+
+INSERT INTO `is_alloted_to` (`t_name`, `S_id`) VALUES
+('Rasika Ransing', 'S16'),
+('Rasika Ransing', 'S17');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `is_from`
+--
+
+CREATE TABLE `is_from` (
+  `D_name` varchar(10) NOT NULL,
+  `t_name` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `is_from`
+--
+
+INSERT INTO `is_from` (`D_name`, `t_name`) VALUES
+('INFT', 'Rasika Ransing'),
+('INFT', 'Rohit Barve');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `selects`
+--
+
+CREATE TABLE `selects` (
+  `Roll_no` varchar(10) NOT NULL,
+  `Subject_name` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `slots`
+--
+
+CREATE TABLE `slots` (
+  `S_id` varchar(5) NOT NULL,
+  `s_time` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `slots`
+--
+
+INSERT INTO `slots` (`S_id`, `s_time`) VALUES
+('S16', '1:45-2:45'),
+('S17', '2:45-3:45');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student`
+--
+
+CREATE TABLE `student` (
+  `Roll_no` varchar(10) NOT NULL,
+  `f_name` varchar(20) DEFAULT NULL,
+  `l_name` varchar(20) DEFAULT NULL,
+  `s_password` varchar(20) DEFAULT NULL,
+  `dept_name` varchar(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `student`
+--
+
+INSERT INTO `student` (`Roll_no`, `f_name`, `l_name`, `s_password`, `dept_name`) VALUES
+('22101b0008', 'Chinmay', 'Tikole', 'Chinmay@345', 'INFT'),
+('22101B0030', 'Aniket', 'panchal', 'Aniket@123', 'INFT'),
+('22104B0060', 'Aditya', 'Vemuri', 'Aditya@123', 'INFT');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student_selects_slots`
+--
+
+CREATE TABLE `student_selects_slots` (
+  `Roll_no` varchar(10) DEFAULT NULL,
+  `S_id` varchar(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `student_selects_slots`
+--
+
+INSERT INTO `student_selects_slots` (`Roll_no`, `S_id`) VALUES
+('22101B0030', 'S16'),
+('22101B0030', 'S17'),
+('22101B0008', 'S16'),
+('22101B0008', 'S17'),
+('22104B0060', 'S16'),
+('22104B0060', 'S17');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `subject`
+--
+
+CREATE TABLE `subject` (
+  `subject_name` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `subject`
+--
+
+INSERT INTO `subject` (`subject_name`) VALUES
+('Database Management'),
+('Operating Systems');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sub_has_slots`
+--
+
+CREATE TABLE `sub_has_slots` (
+  `sub_name` varchar(20) DEFAULT NULL,
+  `S_id` varchar(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `sub_has_slots`
+--
+
+INSERT INTO `sub_has_slots` (`sub_name`, `S_id`) VALUES
+('Operating Systems', 'S16'),
+('Operating Systems', 'S17');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `taught_by`
+--
+
+CREATE TABLE `taught_by` (
+  `t_name` varchar(20) NOT NULL,
+  `Subject_name` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `taught_by`
+--
+
+INSERT INTO `taught_by` (`t_name`, `Subject_name`) VALUES
+('Rasika Ransing', 'Operating Systems'),
+('Rohit Barve', 'Database Management');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `teacher`
+--
+
+CREATE TABLE `teacher` (
+  `t_name` varchar(20) NOT NULL,
+  `A_name` varchar(20) DEFAULT NULL,
+  `C_name` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `teacher`
+--
+
+INSERT INTO `teacher` (`t_name`, `A_name`, `C_name`) VALUES
+('Rasika Ransing', NULL, NULL),
+('Rohit Barve', 'Rohit Barve', NULL);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`A_name`);
+
+--
+-- Indexes for table `coordinator`
+--
+ALTER TABLE `coordinator`
+  ADD PRIMARY KEY (`C_name`);
+
+--
+-- Indexes for table `department`
+--
+ALTER TABLE `department`
+  ADD PRIMARY KEY (`dept_name`);
+
+--
+-- Indexes for table `dept_contains`
+--
+ALTER TABLE `dept_contains`
+  ADD PRIMARY KEY (`D_name`,`Subject_name`),
+  ADD KEY `Subject_name` (`Subject_name`);
+
+--
+-- Indexes for table `is_alloted_to`
+--
+ALTER TABLE `is_alloted_to`
+  ADD KEY `t_name` (`t_name`),
+  ADD KEY `S_id` (`S_id`);
+
+--
+-- Indexes for table `is_from`
+--
+ALTER TABLE `is_from`
+  ADD PRIMARY KEY (`D_name`,`t_name`),
+  ADD KEY `t_name` (`t_name`);
+
+--
+-- Indexes for table `selects`
+--
+ALTER TABLE `selects`
+  ADD PRIMARY KEY (`Roll_no`,`Subject_name`),
+  ADD KEY `Subject_name` (`Subject_name`);
+
+--
+-- Indexes for table `slots`
+--
+ALTER TABLE `slots`
+  ADD PRIMARY KEY (`S_id`);
+
+--
+-- Indexes for table `student`
+--
+ALTER TABLE `student`
+  ADD PRIMARY KEY (`Roll_no`),
+  ADD KEY `dept_name` (`dept_name`);
+
+--
+-- Indexes for table `student_selects_slots`
+--
+ALTER TABLE `student_selects_slots`
+  ADD KEY `Roll_no` (`Roll_no`),
+  ADD KEY `S_id` (`S_id`);
+
+--
+-- Indexes for table `subject`
+--
+ALTER TABLE `subject`
+  ADD PRIMARY KEY (`subject_name`);
+
+--
+-- Indexes for table `sub_has_slots`
+--
+ALTER TABLE `sub_has_slots`
+  ADD KEY `sub_name` (`sub_name`),
+  ADD KEY `S_id` (`S_id`);
+
+--
+-- Indexes for table `taught_by`
+--
+ALTER TABLE `taught_by`
+  ADD PRIMARY KEY (`t_name`,`Subject_name`),
+  ADD KEY `Subject_name` (`Subject_name`);
+
+--
+-- Indexes for table `teacher`
+--
+ALTER TABLE `teacher`
+  ADD PRIMARY KEY (`t_name`),
+  ADD KEY `A_name` (`A_name`),
+  ADD KEY `C_name` (`C_name`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `dept_contains`
+--
+ALTER TABLE `dept_contains`
+  ADD CONSTRAINT `dept_contains_ibfk_1` FOREIGN KEY (`D_name`) REFERENCES `department` (`dept_name`),
+  ADD CONSTRAINT `dept_contains_ibfk_2` FOREIGN KEY (`Subject_name`) REFERENCES `subject` (`subject_name`);
+
+--
+-- Constraints for table `is_alloted_to`
+--
+ALTER TABLE `is_alloted_to`
+  ADD CONSTRAINT `is_alloted_to_ibfk_1` FOREIGN KEY (`t_name`) REFERENCES `teacher` (`t_name`),
+  ADD CONSTRAINT `is_alloted_to_ibfk_2` FOREIGN KEY (`S_id`) REFERENCES `slots` (`S_id`);
+
+--
+-- Constraints for table `is_from`
+--
+ALTER TABLE `is_from`
+  ADD CONSTRAINT `is_from_ibfk_1` FOREIGN KEY (`D_name`) REFERENCES `department` (`dept_name`),
+  ADD CONSTRAINT `is_from_ibfk_2` FOREIGN KEY (`t_name`) REFERENCES `teacher` (`t_name`);
+
+--
+-- Constraints for table `selects`
+--
+ALTER TABLE `selects`
+  ADD CONSTRAINT `selects_ibfk_1` FOREIGN KEY (`Roll_no`) REFERENCES `student` (`Roll_no`),
+  ADD CONSTRAINT `selects_ibfk_2` FOREIGN KEY (`Subject_name`) REFERENCES `subject` (`subject_name`);
+
+--
+-- Constraints for table `student`
+--
+ALTER TABLE `student`
+  ADD CONSTRAINT `student_ibfk_1` FOREIGN KEY (`dept_name`) REFERENCES `department` (`dept_name`);
+
+--
+-- Constraints for table `student_selects_slots`
+--
+ALTER TABLE `student_selects_slots`
+  ADD CONSTRAINT `student_selects_slots_ibfk_1` FOREIGN KEY (`Roll_no`) REFERENCES `student` (`Roll_no`),
+  ADD CONSTRAINT `student_selects_slots_ibfk_2` FOREIGN KEY (`S_id`) REFERENCES `slots` (`S_id`);
+
+--
+-- Constraints for table `sub_has_slots`
+--
+ALTER TABLE `sub_has_slots`
+  ADD CONSTRAINT `sub_has_slots_ibfk_1` FOREIGN KEY (`sub_name`) REFERENCES `subject` (`subject_name`),
+  ADD CONSTRAINT `sub_has_slots_ibfk_2` FOREIGN KEY (`S_id`) REFERENCES `slots` (`S_id`);
+
+--
+-- Constraints for table `taught_by`
+--
+ALTER TABLE `taught_by`
+  ADD CONSTRAINT `taught_by_ibfk_1` FOREIGN KEY (`t_name`) REFERENCES `teacher` (`t_name`),
+  ADD CONSTRAINT `taught_by_ibfk_2` FOREIGN KEY (`Subject_name`) REFERENCES `subject` (`subject_name`);
+
+--
+-- Constraints for table `teacher`
+--
+ALTER TABLE `teacher`
+  ADD CONSTRAINT `teacher_ibfk_1` FOREIGN KEY (`A_name`) REFERENCES `admin` (`A_name`),
+  ADD CONSTRAINT `teacher_ibfk_2` FOREIGN KEY (`C_name`) REFERENCES `coordinator` (`C_name`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
