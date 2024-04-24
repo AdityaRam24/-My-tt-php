1fa558b079df82adab23fafd6a0b8764a4ad378f
AdityaRam24 <89917849+AdityaRam24@users.noreply.github.com>
2024-04-25 01:20:17 +0530

    Delete MiniProject directory

--- MiniProject/giv.php
+++
@@ -1,5 +0,0 @@
-<?php
-session_start();
-$_SESSION['YO'] = 'YO';
-//var_dump($_SESSION); // Debugging statement
-?>
--- MiniProject/rec.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-//var_dump($_SESSION); // Debugging statement
-echo $_SESSION['YO'];
-
-?>
